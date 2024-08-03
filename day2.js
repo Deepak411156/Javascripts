6914e705bf49e19d4af3800adebf14ac045bc860
Deepak411156 <dpkbhatta2051@gmail.com>
2024-08-03 19:09:56 +0545

    LKearn about variable, array, object

---
+++ day2.js
@@ -0,0 +1,43 @@
+var glass1 = "paani";
+let glass2 = "paani";
+const glass3 = "paani";
+
+const gamala = "full";
+
+var secret = "password";
+
+// const isNepali = ture;
+
+const friend1 ="Deepak"
+const friend2 = "Harish"
+const friend3 = "Pushpa"
+
+const friends =["Deepak","Harish","Pushpa"]
+
+
+const shoe1 = "Addidas"
+const shoe2 = "Puma"
+const shoe3 ="Umbro"
+
+const shoes = ["Addidas","Puma","Umbro"]
+
+console.log(shoes);
+
+
+const name = "Deepak Bhatta";
+const age = 30;
+const address = "Dhangadhi"
+const isNepali2 = true
+
+// Object data Type KEY : VALUE
+
+const me ={
+    name : "Deepak Bhatta",
+    age : 30,
+    address : "Dhangadhi",
+    isNepali :true
+}
+
+// const obj {
+//     key : value
+// }
